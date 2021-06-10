9e3292805573b8008f94a3101d7aaac7d0ee7ddc
Ana <annagabsoares@gmail.com>
2021-06-10 10:04:19 -0700

    changes on index.js

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
+const buttons = document.querySelectorAll('button')
+let audio = new Audio("/Users/gabi/Desktop/DrumKit/sounds/crash.mp3");
 
-document.querySelector('button').addEventListener('click', handleClick)
-
-function handleClick() {
-    alert('I got clicked')
+for (const button of buttons){
+    if (button.classList == "w drum"){
+        button.addEventListener('click', function(){
+        let audio = new Audio("/Users/gabi/Desktop/DrumKit/sounds/tom-1.mp3")
+        audio.play()})
+    }
+    if (button.classList == "a drum"){
+        button.addEventListener('click', function(){
+        let audio = new Audio("/Users/gabi/Desktop/DrumKit/sounds/tom-2.mp3")
+        audio.play()})
+    }
+    if (button.classList == "s drum"){
+        button.addEventListener('click', function(){
+        let audio = new Audio("/Users/gabi/Desktop/DrumKit/sounds/tom-3.mp3")
+        audio.play()})
+    }
+    if (button.classList == "d drum"){
+        button.addEventListener('click', function(){
+        let audio = new Audio("/Users/gabi/Desktop/DrumKit/sounds/tom-1.mp3")
+        audio.play()})
+    }
+    if (button.classList == "j drum"){
+        button.addEventListener('click', function(){
+        let audio = new Audio("/Users/gabi/Desktop/DrumKit/sounds/snare.mp3")
+        audio.play()})
+    }
+    if (button.classList == "k drum"){
+        button.addEventListener('click', function(){
+        let audio = new Audio("/Users/gabi/Desktop/DrumKit/sounds/snare.mp3")
+        audio.play()})
+    }
+    if (button.classList == "l drum"){
+        button.addEventListener('click', function(){
+        let audio = new Audio("/Users/gabi/Desktop/DrumKit/sounds/kick-bass.mp3")
+        audio.play()})
+    }  
 }
